fdfe995c3c817656b3c1d1129308e9bd41a42045
Guilherme Ramalho <guilhermekr.x@gmail.com>
2020-02-28 16:35:42 -0300

    Hora da reserva configurada

--- app/src/main/java/com/guilherme/getherfy/reserva/adapter/ListaReservasAdapter.java
+++ app/src/main/java/com/guilherme/getherfy/reserva/adapter/ListaReservasAdapter.java
@@ -22,7 +22,9 @@ import com.guilherme.getherfy.httpService.HttpServiceReservasByOrganizacao;
 import com.guilherme.getherfy.reserva.model.Reserva;
 import com.guilherme.presentation.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -67,34 +69,28 @@ public class ListaReservasAdapter extends BaseAdapter {
     public View getView(final int position, View convertView, final ViewGroup parent) {
 
 
-
         final View viewCriada = LayoutInflater.from(context)
                 .inflate(R.layout.item_reserva_layout, parent, false);
         final Reserva reserva = reservas.get(position);
 
+        TextView data = viewCriada.findViewById(R.id.fragment_item_reserva_dia);
+        TextView horaInicio = viewCriada.findViewById(R.id.fragment_item_reserva_hora_inicio);
+        TextView horaFim = viewCriada.findViewById(R.id.fragment_item_reserva_hora_fim);
 
         TextView descricao = viewCriada.findViewById(R.id.fragmente_item_reserva_assunto);
         descricao.setText(reserva.getDescricao());
-
-
         TextView organizador = viewCriada.findViewById(R.id.fragment_item_reserva_organizador);
         organizador.setText(reserva.getNomeOrganizador());
-
         TextView localizacao = viewCriada.findViewById(R.id.fragment_item_reserva_localizacao);
         localizacao.setText(reserva.getNomeSala());
-
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
-        TextView data = viewCriada.findViewById(R.id.fragment_item_reserva_dia);
-        data.setText(reserva.getDataHoraInicio());
 
 
         CardView cardView = viewCriada.findViewById(R.id.item_reserva_cardview);
         final LinearLayout linearLayout = viewCriada.findViewById(R.id.item_reserva_layout_options);
 
-        if(position == selecionado)
-        {
+        configuraDataHora(reserva, data, horaInicio, horaFim);
+
+        if (position == selecionado) {
             linearLayout.setVisibility(View.VISIBLE);
 
             ImageButton btnRemover = viewCriada.findViewById(R.id.fragment_item_reserva_remover);
@@ -116,8 +112,8 @@ public class ListaReservasAdapter extends BaseAdapter {
             btnRemover.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                   linearLayout.setVisibility(View.GONE);
-                   String idReserva = String.valueOf(reserva.getId());
+                    linearLayout.setVisibility(View.GONE);
+                    String idReserva = String.valueOf(reserva.getId());
                     ListaReservasFragment listaReservasFragment = new ListaReservasFragment();
 
                     try {
@@ -136,9 +132,7 @@ public class ListaReservasAdapter extends BaseAdapter {
                     reserva.setAtivo(false);
                 }
             });
-        }
-        else
-        {
+        } else {
             linearLayout.setVisibility(View.GONE);
 
         }
@@ -150,18 +144,54 @@ public class ListaReservasAdapter extends BaseAdapter {
                 Log.e("Adapter", "Valor: " + selecionado);
                 atualizaLista.atualizarLista(true);
 
-
                 return false;
-
-
             }
 
         });
 
-
-
-
         return viewCriada;
+    }
+
+    public void configuraDataHora(Reserva reserva, TextView data, TextView horaInicio, TextView horaFim) {
+        SimpleDateFormat dateFormatUS = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat dateFormatBR = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat hourFormatRecebe = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat hourFormat = new SimpleDateFormat("HH:mm");
+
+        for (int i = 0; i < 2; i++) {
+            String dataHora;
+
+            if (i == 0) {
+                dataHora = reserva.getDataHoraInicio();
+            } else {
+                dataHora = reserva.getDataHoraFim();
+            }
+            String[] dataSplitada = dataHora.split("T");
+            String[] horaInicioSplitada = dataSplitada[1].split("Z");
+
+
+            try {
+                Date dateParseado = dateFormatUS.parse(dataSplitada[0]);
+                String dateStr = dateFormatBR.format(dateParseado);
+                Date horaInicioParseada = hourFormatRecebe.parse(horaInicioSplitada[0]);
+                String horaStr = hourFormat.format(horaInicioParseada);
+
+                System.out.println(dataSplitada[1]);
+
+
+                data.setText(dateStr);
+                System.out.println(data.getText().toString());
+
+                if (i == 0) {
+                    horaInicio.setText(horaStr + " - ");
+                } else {
+                    horaFim.setText(horaStr);
+                }
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
